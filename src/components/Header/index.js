a9fac9d01745fe2407ed741e68c56f2a43b3be3f
CristinaBradu <cristina_bradu@mail.ru>
2019-01-28 19:18:48 +0200

    Initial commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,87 @@
+import React from 'react'
+import logo from './logo.png'
+import './header.css'
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  InputGroup,
+  InputGroupAddon,
+  Input,
+  FormText,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  ButtonDropdown,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap'
+
+export default class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.toggle = this.toggle.bind(this)
+    this.state = {
+      isOpen: false
+    }
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">
+            <div className="logo">
+              <img src={logo} alt="Logo" />
+            </div>
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <Form className="search">
+                <FormGroup>
+                  <Input
+                    type="search"
+                    name="search"
+                    id="search"
+                    placeholder="Search..."
+                  />
+                </FormGroup>
+              </Form>
+              <ButtonDropdown
+                isOpen={this.state.isOpen}
+                toggle={this.toggle}
+                className="lang">
+                <DropdownToggle caret size="sm">
+                  En
+                </DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem>Ro</DropdownItem>
+                  <DropdownItem>Rus</DropdownItem>
+                </DropdownMenu>
+              </ButtonDropdown>
+              <Button outline color="info" className="register">
+                Register
+              </Button>
+              <Button outline color="success" className="login">
+                Log In
+              </Button>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    )
+  }
+}
